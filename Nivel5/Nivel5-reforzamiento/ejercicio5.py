0d274eece9c874471139b2c9c2c0c108134d0119
Alexisortega <manuellokote77@gmail.com>
2026-04-03 19:19:37 -0700

    refuerzos

--- Nivel5/Nivel5-reforzamiento/ejercicio5.py
+++ Nivel5/Nivel5-reforzamiento/ejercicio5.py
@@ -14,3 +14,47 @@ todo.by_priority()
 → [P2] Hacer ejercicio ✗
 → [P3] Leer libro ✗
 """
+class TodoList:
+    def __init__(self):
+        self.tasks=[]
+    
+    def add_task(self,title,priority ):
+        task={
+            "title":title,
+            "priority":priority,
+            "complete":False
+        }
+        self.tasks.append(task)    
+    # ✅ Devuelve confirmación
+    def complete_task(self, title):
+     for task in self.tasks:
+        if task['title'] == title:
+            task['complete'] = True
+            return f"'{title}' marked as complete."
+     return f"Task '{title}' not found."
+
+    def pending(self):
+        pendings = [task['title'] for task in self.tasks if not task['complete']]
+        return pendings
+         
+    def by_priority(self):
+        ordenadas = sorted(self.tasks, key=lambda x: x['priority'])
+        for task in ordenadas:
+            estado = "✓" if task['complete'] else "✗"
+            print(f"[P{task['priority']}] {task['title']} {estado}")
+    
+    def __str__(self):
+        resultado = ""
+        for task in self.tasks:
+            estado = "✓" if task['complete'] else "✗"
+            resultado += f"[P{task['priority']}] {task['title']} {estado}\n"
+        return resultado
+    
+todo = TodoList()
+todo.add_task("Estudiar Python", 1)
+todo.add_task("Hacer ejercicio", 2)
+todo.add_task("Leer libro", 3)
+print(todo.complete_task("Estudiar Python"))
+print(todo.pending() )   #  → ["Hacer ejercicio", "Leer libro"]
+print(todo.by_priority())
+print(todo)

d0147da5e79e602f5a8d9b746914141941581de1
Alexisortega <manuellokote77@gmail.com>
2026-04-23 22:59:40 -0700

    exercise 1 and 2 finished

--- Decoradores/ejercicio1.py
+++ Decoradores/ejercicio1.py
@@ -1,9 +1,23 @@
 """
 Ejercicio 1 — Fácil
 Crea un decorador uppercase que convierta a mayúsculas el string que devuelve una función.
-python@uppercase
+python
+@uppercase
 def greet(name):
     return f"hello, {name}!"
 
 greet("manuel")   → "HELLO, MANUEL!"
 """
+from functools import wraps
+
+def uppercase(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result.upper()
+    return wrapper
+@uppercase
+def greet(name):
+    return f"hello, {name}!"
+
+print(greet("manuel"))
--- Decoradores/ejercicio2.py
+++ Decoradores/ejercicio2.py
@@ -1,7 +1,8 @@
 """
 Ejercicio 2 — Fácil
 Crea un decorador timer que imprima cuánto tardó en ejecutarse la función.
-python@timer
+python
+@timer
 def slow_sum(n):
     return sum(range(n))
 
@@ -10,3 +11,23 @@ slow_sum(1000000)
 # → 499999500000
 
 """
+import time
+from functools import wraps
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start  = time.time()
+        result = func(*args, **kwargs)
+        end    = time.time()
+        print(f"{func.__name__} took {end - start:.4f} seconds")
+        return result
+    return wrapper
+
+@timer
+def slow_sum(n):
+    return sum(range(n))
+
+print(slow_sum(1000000))
+
+
+        
